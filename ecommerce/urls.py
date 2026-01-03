a3d2450fac428e94b462e7bff7c65608e3b36ea1
Sharipov Jonibek <jsharipov559@gmail.com>
2026-01-03 18:26:53 +0500

    add cart app

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,8 +20,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path,include
 urlpatterns = [
+    # Admin url
     path('admin/', admin.site.urls),
+
+    # Store url
     path('',include('store.urls')),
+
+    # Cart url
+    path('cart/',include('cart.urls')),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
