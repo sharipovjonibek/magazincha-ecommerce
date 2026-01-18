baec10272382e10d0c73713ec05750c79cb3fb7d
Sharipov Jonibek <jsharipov559@gmail.com>
2026-01-18 20:50:34 +0500

    Work on 'Payment Integration' (with PayPal)

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -160,3 +160,8 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
+# Allow PayPal Popups
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import ShippingAddress,Order,OrderItem
 from cart.cart import Cart
 from django.http import JsonResponse
+
+from django.core.mail import send_mail
+from django.conf import settings
+
 # Create your views here.
 
 def checkout(request):
@@ -77,11 +81,21 @@ def complete_order(request):
 
             order_id = order.pk
 
+            product_list = []
+
             for item in cart:
 
                 OrderItem.objects.create(order_id=order_id,product=item['product'],quantity=item['qty'],
                 
                 price=item['price'],user = request.user)
+
+                product_list.append(item['product'].__str__())
+            
+            all_products = product_list
+            
+            send_mail('Order received','Hi!' + '\n\n'+'Thank you for placing your order'+'\n\n'+
+                      'Please see your order below:'+'\n\n'+str(all_products)+'\n\n'+'Total paid: $'+str(cart.get_total()),settings.EMAIL_HOST_USER,[email],fail_silently=False
+                      )
 
             
         # 2) Create order -> Guest users without an account
@@ -94,11 +108,23 @@ def complete_order(request):
 
             order_id = order.pk
 
+            product_list = []
+
             for item in cart:
 
                 OrderItem.objects.create(order_id=order_id,product=item['product'],quantity=item['qty'],
                 
                 price=item['price'],user = None)
+
+                product_list.append(item['product'].__str__())
+
+            all_products = product_list
+
+            # Email order
+
+            send_mail('Order received','Hi!' + '\n\n'+'Thank you for placing your order'+'\n\n'+
+                      'Please see your order below:'+'\n\n'+str(all_products)+'\n\n'+'Total paid: $'+str(cart.get_total()),settings.EMAIL_HOST_USER,[email],fail_silently=False
+                      )
 
 
         order_success = True
